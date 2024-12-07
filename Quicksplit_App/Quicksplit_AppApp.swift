a9a0d65275239079dea5c2e7e041d67b3312de4b
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-12-07 10:15:02 +0530

    Initial Commit

---
+++ Quicksplit_App/Quicksplit_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quicksplit_AppApp.swift
+//  Quicksplit_App
+//
+//  Created by Thushini Abeysuriya on 2024-12-07.
+//
+
+import SwiftUI
+
+@main
+struct Quicksplit_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Quicksplit_AppTests/Quicksplit_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Quicksplit_AppTests.swift
+//  Quicksplit_AppTests
+//
+//  Created by Thushini Abeysuriya on 2024-12-07.
+//
+
+import Testing
+@testable import Quicksplit_App
+
+struct Quicksplit_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
